c7717d3d8319e076a9f1a0bd9f4c8fbbf0fefd69
GamzeBilmez <C9019274@arcelik.com>
2023-05-19 20:00:46 +0300

    entitiy ve model alanları güncellendi.

--- quinoaMarketApp/src/main/java/com/graduationproject/quinoamarketapp/entity/Product.java
+++ quinoaMarketApp/src/main/java/com/graduationproject/quinoamarketapp/entity/Product.java
@@ -1,7 +1,9 @@
 package com.graduationproject.quinoamarketapp.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "PRODUCT")
 public class Product {
--- quinoaMarketApp/src/main/java/com/graduationproject/quinoamarketapp/model/ProductRequestDTO.java
+++ quinoaMarketApp/src/main/java/com/graduationproject/quinoamarketapp/model/ProductRequestDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class ProductRequestDTO {
+    private Long id;
     private Long farmerId;
     private ImageFile imageFile;
     private String info;
