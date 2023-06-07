ecb68cca4193832ed363a0dbe1964e85822c8877
Esref Burak Kaya <ebkaya.0637@gmail.com>
2023-06-07 23:23:59 +0300

    login endpoint is created

--- quinoaMarketApp/src/main/java/com/graduationproject/quinoamarketapp/QuinoaMarketAppApplication.java
+++ quinoaMarketApp/src/main/java/com/graduationproject/quinoamarketapp/QuinoaMarketAppApplication.java
@@ -1,6 +1,5 @@
 package com.graduationproject.quinoamarketapp;
 
-import com.graduationproject.quinoamarketapp.test.ModelTester;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,12 +10,6 @@ public class QuinoaMarketAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuinoaMarketAppApplication.class, args);
-
-        /*try {
-            ModelTester.testModel();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }*/
 
     }
 
--- quinoaMarketApp/src/main/java/com/graduationproject/quinoamarketapp/repository/FarmerRepository.java
+++ quinoaMarketApp/src/main/java/com/graduationproject/quinoamarketapp/repository/FarmerRepository.java
@@ -4,4 +4,6 @@ import com.graduationproject.quinoamarketapp.entity.Farmer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FarmerRepository extends JpaRepository<Farmer,Long> {
+    boolean existsByEmailAndPassword(String email, String password);
+    Farmer findByEmail(String email);
 }
