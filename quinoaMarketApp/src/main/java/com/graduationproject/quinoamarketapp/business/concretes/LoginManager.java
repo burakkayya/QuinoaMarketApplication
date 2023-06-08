032402f073bffa9c8eaa3ba8c6efdfb7e87e51d2
Esref Burak Kaya <ebkaya.0637@gmail.com>
2023-06-09 00:23:38 +0300

    get logged in user by session is added

--- quinoaMarketApp/src/main/java/com/graduationproject/quinoamarketapp/business/concretes/LoginManager.java
+++ quinoaMarketApp/src/main/java/com/graduationproject/quinoamarketapp/business/concretes/LoginManager.java
@@ -4,9 +4,13 @@ import com.graduationproject.quinoamarketapp.business.abstracts.FarmerService;
 import com.graduationproject.quinoamarketapp.business.abstracts.LoginService;
 import com.graduationproject.quinoamarketapp.model.LoginRequestDTO;
 import com.graduationproject.quinoamarketapp.model.LoginResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @AllArgsConstructor
 @Service
@@ -14,9 +18,13 @@ public class LoginManager implements LoginService {
     FarmerService farmerService;
     ModelMapper modelMapper;
     @Override
-    public LoginResponseDTO login(LoginRequestDTO request) {
-            if(farmerService.existsByEmailAndPassword(request)){
-                return modelMapper.map(farmerService.findByEmail(request.getEmail()), LoginResponseDTO.class);
+    public LoginResponseDTO login(LoginRequestDTO loginRequest) {
+            if(farmerService.existsByEmailAndPassword(loginRequest)){
+                HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+                HttpSession session = request.getSession();
+                session.setAttribute("email", loginRequest.getEmail());
+
+                return modelMapper.map(farmerService.findByEmail(loginRequest.getEmail()), LoginResponseDTO.class);
             }else {
                 throw new RuntimeException("email veya şifre hatalı!");
             }
