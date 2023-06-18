377d7666e33c7bf6927f9e55a09467ebb5c79b95
Ezgi Özdikyar <ezgiozdikyar@gmail.com>
2023-06-18 12:19:17 +0300

    Added error message for home and popular sellers page

--- quinoa-market-app-ui/src/HomePage.js
+++ quinoa-market-app-ui/src/HomePage.js
@@ -7,15 +7,26 @@ import UserNavbar from "./UserNavbar";
 
 function HomePage() {
     const [products, setProducts] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
     const email = sessionStorage.getItem('email');
     
+    useEffect(() => {
+        if (errorMessage) {
+            const timer = setTimeout(() => {
+                setErrorMessage('');
+            }, 2000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [errorMessage]);
+
     useEffect(() => {
         axios.get("/api/products/home-products")
             .then(response => {
                 setProducts(response.data);
             })
             .catch(error => {
-                console.error("Error fetching products:", error);
+                setErrorMessage("An error occured while receiving products");
             });
     }, []);
     
@@ -23,6 +34,7 @@ function HomePage() {
         <>
             {email ? <UserNavbar /> : <CustomNavbar />}
             <div className="container">
+                <div className={`error-message ${errorMessage ? 'show' : ''}`}>{errorMessage}</div>
                 {Array.from({ length: Math.ceil(products.length / 3) }).map((_, rowIndex) => (
                     <div key={rowIndex} className="row">
                         {products.slice(rowIndex * 3, rowIndex * 3 + 3).map((product) => (
